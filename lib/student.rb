5f40b3b51ca6bd2f15ba708a4f1cfe27a88c2e2c
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-08-06 01:36:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,4 +6,21 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
+
 end
